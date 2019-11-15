0a5a7af0779b60cd9bb3d22718f7cc1642af996c
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2019-11-15 10:47:38 +0300

    Create preprocess_cos_similarity.py

---
+++ Face Matching/preprocess_cos_similarity.py
@@ -0,0 +1,16 @@
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.imagenet_utils import preprocess_input
+
+def preprocess_image(image_path):
+    img = load_img(image_path, target_size=(224, 224))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+def findCosineSimilarity(source_representation, test_representation):
+    a = np.matmul(np.transpose(source_representation), test_representation)
+    b = np.sum(np.multiply(source_representation, source_representation))
+    c = np.sum(np.multiply(test_representation, test_representation))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
